c8942f7a3987d86d35887071b04c06ed4d8b1c11
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2024-10-15 01:21:37 +0200

    eslint & prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,19 +3,41 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: [
+      'dist',
+      'node_modules/',
+      'package.json',
+      'package-lock.json',
+      '.eslintrc.json',
+      'tsconfig.json',
+      'next.config.mjs'
+    ],
+    extends: [
+      'plugin:prettier/recommended',
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-type-checked',
+      'plugin:storybook/recommended',
+      js.configs.recommended,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: './tsconfig.json',
+    },
     plugins: {
+      '@typescript-eslint': tseslint,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +45,25 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/require-await': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+      '@typescript-eslint/no-inferrable-types': 'error',
+      '@typescript-eslint/typedef': [
+        'error',
+        {
+          arrayDestructuring: false,
+          arrowParameter: false,
+          memberVariableDeclaration: false,
+          objectDestructuring: false,
+          parameter: false,
+          propertyDeclaration: false,
+          variableDeclaration: false,
+          variableDeclarationIgnoreFunction: false,
+        },
+      ],
+      'prettier/prettier': 'error'
     },
-  },
+  }
 )
