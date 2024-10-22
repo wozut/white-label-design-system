32d544d59982b4b15d11176f4d313676c5503d41
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2024-10-23 00:05:37 +0200

    change

--- lib/components/Button/index.tsx
+++ lib/components/Button/index.tsx
@@ -1,6 +1,16 @@
 import React, { ReactElement, ReactNode } from "react"
 import styles from "./styles.module.css"
 
-export function PrimaryButton({ onClick, children }: { onClick?: React.MouseEventHandler<HTMLButtonElement> ; children: ReactNode }): ReactElement {
-  return <button className={styles["button"]} onClick={onClick}>{children}</button>
+export function PrimaryButton({
+  onClick,
+  children,
+}: {
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
+  children: ReactNode
+}): ReactElement {
+  return (
+    <button className={styles["button"]} onClick={onClick}>
+      {children}
+    </button>
+  )
 }
