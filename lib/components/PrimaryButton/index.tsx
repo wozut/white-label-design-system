1d61892d3e3aa32c78f58a62e45b54d5e7d3b9b6
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2024-10-24 01:17:39 +0200

    change

--- lib/components/PrimaryButton/index.tsx
+++ lib/components/PrimaryButton/index.tsx
@@ -2,14 +2,16 @@ import React, { ReactElement, ReactNode } from "react"
 import styles from "./styles.module.css"
 
 export function PrimaryButton({
+  type = "button",
   onClick,
   children,
 }: {
+  type?: "submit" | "reset" | "button"
   onClick?: React.MouseEventHandler<HTMLButtonElement>
   children: ReactNode
 }): ReactElement {
   return (
-    <button className={styles["button"]} onClick={onClick}>
+    <button type={type} className={styles["button"]} onClick={onClick}>
       {children}
     </button>
   )
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { ReactElement, useState } from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
-import "./App.css"
-import { PrimaryButton } from "../lib/components/Button"
+import "normalize.css"
+import "../styles/variables.css"
+import "../styles/global.css"
+import { PrimaryButton } from "../lib/components/PrimaryButton"
 import { Label } from "../lib/components/Label"
 
 function App(): ReactElement {
@@ -25,9 +27,9 @@ function App(): ReactElement {
             setCount((count) => count + 1)
           }}
         >
-          count is {count}
+          My Count is {count}
         </PrimaryButton>
-        <Label primary={true}>My Label</Label>
+        <Label primary={true}>My Count is {0 + 1}</Label>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -36,7 +38,7 @@ function App(): ReactElement {
         Click on the Vite and React logos to learn more
       </p>
       <PrimaryButton>My Button</PrimaryButton>
-      <Label primary={true}>My Label</Label>
+      <Label primary={true}>My Count is {0 + 1}</Label>
     </>
   )
 }
